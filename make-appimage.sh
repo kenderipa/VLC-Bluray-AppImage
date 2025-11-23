77d6e1bf830626600bfd7614fbf757324cab0d4d
kenderipa <52325831+kenderipa@users.noreply.github.com>
2025-11-23 22:50:50 +0100

    Refactor make-appimage.sh for VLC integration
    Comment out unused environment variables and update paths for VLC.

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
-export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+#export ADD_HOOKS="self-updater.bg.hook"
+#export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=/usr/share/icons/hicolor/128x128/apps/vlc.png
+export DESKTOP=/usr/share/applications/vlc.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/*vlc*
 
 # Additional changes can be done in between here
 
